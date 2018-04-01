d558fe83224a4e2f7b375ee1f84029cdd66133dd
James Reynolds <jameshreynolds@gmail.com>
2018-04-01 10:50:45 -0700

    Dice greyed out when inactive. Buttons only function when appropriate.

--- src/InPlay/InPlay.js
+++ src/InPlay/InPlay.js
@@ -6,9 +6,11 @@ const InPlay = (props) => (
   <div className='InPlay'>
     {props.inPlay.map((dice, index) => {
       if (props.selected[index] === true) {
-        return <Dice isSelected={true} number={dice} key={index} select={props.select.bind(null, index)} />
+        return <Dice diceActive={true} isSelected={true} number={dice} key={index} select={props.select.bind(null, index)} />
+      } else if (props.diceActive) {
+        return <Dice diceActive={false} isSelected={false} number={dice} key={index} />
       } else {
-        return <Dice isSelected={false} number={dice} key={index} select={props.select.bind(null, index)} />
+        return <Dice diceActive={true} isSelected={false} number={dice} key={index} select={props.select.bind(null, index)} />
       }
     })}
   </div>
